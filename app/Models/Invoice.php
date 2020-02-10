90227b0c120498966d85484067bbc4042982baa5
mohsen <mhsenpc@gmail.com>
2020-02-10 17:35:16 +0330

    Generate Invoice #42

--- app/Models/Invoice.php
+++ app/Models/Invoice.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Scopes\UserIDScope;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
@@ -9,4 +10,11 @@ class Invoice extends Model
 {
     use SoftDeletes;
     protected $guarded = ['id'];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope(new UserIDScope());
+    }
 }
--- app/Services/BillingService.php
+++ app/Services/BillingService.php
@@ -17,6 +17,8 @@ class BillingService
         $users = User::where('last_billing_date', '<', $last_30_days)
             ->get();
 
+        $users = User::all();
+
         foreach ($users as $user) {
             $data = [];
             $total_amount = 0;
@@ -26,48 +28,49 @@ class BillingService
             get();
             //calculate vpses
             foreach ($machines as $machine) {
-                echo $machine->id;
-     /*           if (!empty($machine->deleted_at))
-                    if ($machine->deleted_at->lt($user->last_billing_date))
-                        continue;*/
-                $billings = MachineBilling::where('machine_id', $machine->id)->
-                where('created_at', '>=', $user->last_billing_date)->
+                /*           if (!empty($machine->deleted_at))
+                               if ($machine->deleted_at->lt($user->last_billing_date))
+                                   continue;*/
+                $billings = MachineBilling::where('machine_id', $machine->id)->orderBy('created_at')->
                 get();
                 foreach ($billings as $billing) {
-                    echo $billing->id;
                     $now = Carbon::now();
                     $amount = 0;
                     if (empty($billing->end_date)) {
-                        $hours = $billing->created_at->diffInHours($now);
+                        $hours = $billing->last_billing_date->diffInHours($now);
                         $amount = $hours * $billing->plan->hourly_price;
                         $total_amount += $amount;
                         $end_date = $now;
                     } else {
                         $end_date = new Carbon($billing->end_date);
-                        $hours = $billing->created_at->diffInHours($end_date);
+                        $hours = $billing->last_billing_date->diffInHours($end_date);
                         $amount = $hours * $billing->plan->hourly_price;
                         $total_amount += $amount;
                     }
 
                     $vat = $amount * 0.09;
                     $total_vat += $vat;
-                    $billing_item = [
-                        'title' => 'Server usage period',
-                        'type' => 'machine',
-                        'start' => $billing->created_at,
-                        'end' => $end_date,
-                        'hours' => $hours,
-                        'amount' => $amount,
-                        'vat' => $vat,
-                        'total' => $amount + $vat,
-                        'machine' => $machine,
-                        'plan' => $billing->plan,
-                    ];
-                    $data[] = $billing_item;
-                    //$billing->updateLastBillingDate();
+                    if ($amount > 0) {
+                        $machine_cost_anything = true;
+                        $billing_item = [
+                            'title' => 'Server usage period',
+                            'type' => 'machine',
+                            'start' => $billing->last_billing_date,
+                            'end' => $end_date,
+                            'hours' => $hours,
+                            'amount' => $amount,
+                            'vat' => $vat,
+                            'total' => $amount + $vat,
+                            'machine' => $machine,
+                            'plan' => $billing->plan,
+                        ];
+                        $data[] = $billing_item;
+                        $billing->updateLastBillingDate();
+                    }
+                    else{
+                        $machine->updateLastBillingDate();
+                    }
                 }
-
-                //$machine->updateLastBillingDate();
 
             }
 
@@ -77,16 +80,18 @@ class BillingService
 
             //calculate volumes
 
-            //$user->updateLastBillingDate();
+            $user->updateLastBillingDate();
 
-            Invoice::create([
-                'user_id' => $user->id,
-                'amount' => $total_amount,
-                'vat' => $total_vat,
-                'total' => $total_amount + $total_vat,
-                'is_paid' => false,
-                'data' => json_encode($data)
-            ]);
+            if ($total_amount > 0) {
+                Invoice::create([
+                    'user_id' => $user->id,
+                    'amount' => $total_amount,
+                    'vat' => $total_vat,
+                    'total' => $total_amount + $total_vat,
+                    'is_paid' => false,
+                    'data' => json_encode($data)
+                ]);
+            }
         }
     }
 }
